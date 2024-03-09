f097ebac04f1c7c5e8852cce53faf888b213df8b
TheMhv <mhv.geral@gmail.com>
2024-03-09 02:23:26 -0300

    Fix: Menu tabs size

--- src/Pages/Home/components/MenuTabs.jsx
+++ src/Pages/Home/components/MenuTabs.jsx
@@ -4,30 +4,25 @@ import { LuBook, LuHourglass, LuLightbulb, LuSchool } from "react-icons/lu";
 class MenuTabs extends React.Component {
   render() {
     return (
-      <div className="flex text-sm font-medium text-center">
-        <div>
-          <div className="flex items-center py-3 px-5 border-b-2 border-transparent rounded-t-lg hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
-            <LuBook className="mr-1" />
-            About me
-          </div>
+      <div className="flex justify-between text-sm font-medium">
+        <div className="flex items-center justify-center bg-white border-b-2 border-transparent rounded-t-lg py-3 px-5 w-full hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
+          <LuBook className="mr-1" />
+          About me
         </div>
-        <div>
-          <div className="flex items-center py-3 px-5 border-b-2 border-transparent rounded-t-lg hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
-            <LuHourglass className="mr-1" />
-            Experiences
-          </div>
+
+        <div className="flex items-center justify-center bg-white border-b-2 border-transparent rounded-t-lg py-3 px-5 w-full hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
+          <LuHourglass className="mr-1" />
+          Experiences
         </div>
-        <div>
-          <div className="flex items-center py-3 px-5 border-b-2 border-transparent rounded-t-lg hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
-            <LuLightbulb className="mr-1" />
-            Skills
-          </div>
+
+        <div className="flex items-center justify-center bg-white border-b-2 border-transparent rounded-t-lg py-3 px-5 w-full hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
+          <LuLightbulb className="mr-1" />
+          Skills
         </div>
-        <div>
-          <div className="flex items-center py-3 px-5 border-b-2 border-transparent rounded-t-lg hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
-            <LuSchool className="mr-1" />
-            Education
-          </div>
+
+        <div className="flex items-center justify-center bg-white border-b-2 border-transparent rounded-t-lg py-3 px-5 w-full hover:font-bold hover:border-gray-500 active:text-blue-600 active:border-blue-600">
+          <LuSchool className="mr-1" />
+          Education
         </div>
       </div>
     );
