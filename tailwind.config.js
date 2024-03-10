28e4a747e56262c4392d77d49d87efec662a884c
TheMhv <mhv.geral@gmail.com>
2024-03-09 22:13:29 -0300

    Feat: Set colors params

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "var(--color-primary)",
+      secondary: "var(--color-secondary)",
+      success: "var(--color-success)",
+      danger: "var(--color-danger)",
+      warning: "var(--color-warning)",
+      info: "var(--color-info)",
+      light: "var(--color-light)",
+      dark: "var(--color-dark)",
+      ...colors,
+    },
     extend: {},
   },
   plugins: [],
