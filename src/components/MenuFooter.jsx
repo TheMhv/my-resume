beacc8204f40c959d7f06b6d7dc47a2a435d09c3
TheMhv <mhv.geral@gmail.com>
2024-03-09 22:43:02 -0300

    Feat: Add input that change primary color for test

--- src/components/MenuFooter.jsx
+++ src/components/MenuFooter.jsx
@@ -4,15 +4,36 @@ class MenuFooter extends React.Component {
   render() {
     return (
       <div class="absolute bottom-0 bg-gray-300 rounded-r-lg rounded-t-none w-full p-3 mt-10">
-        <span className="text-xs text-nowrap">
-          Created by:{" "}
-          <a
-            href="https://github.com/TheMhv"
-            className="font-semibold hover:underline hover:font-bold"
-          >
-            @TheMhv
-          </a>
-        </span>
+        <div className="flex justify-between items-center">
+          <div>
+            <span className="text-xs text-nowrap">
+              Created by:{" "}
+              <a
+                href="https://github.com/TheMhv"
+                className="font-semibold hover:underline hover:font-bold"
+              >
+                @TheMhv
+              </a>
+            </span>
+          </div>
+
+          <div className="flex items-center">
+            <label>Primary color:</label>
+            <input
+              className="rounded ml-2"
+              type="color"
+              defaultValue={getComputedStyle(
+                document.documentElement
+              ).getPropertyValue("--color-primary")}
+              onChange={(e) => {
+                document.documentElement.style.setProperty(
+                  "--color-primary",
+                  e.target.value
+                );
+              }}
+            />
+          </div>
+        </div>
       </div>
     );
   }
