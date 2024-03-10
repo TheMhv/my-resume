811ef008950679a516ff44e62c6d3d1011d57c79
TheMhv <mhv.geral@gmail.com>
2024-03-10 04:00:37 -0300

    Chore/Fix: Fix contact infos margin on big displays

--- src/components/LeftMenu.jsx
+++ src/components/LeftMenu.jsx
@@ -28,90 +28,92 @@ class LeftMenu extends React.Component {
 
         <hr className="border-1 border-secondary my-5" />
 
-        <div className="space-y-3">
-          <div className="flex items-center">
-            <LuUser className="mr-1" />
-            <span>Name:</span>
-            <span className="font-semibold text-primary ml-2">
-              Luiz F. G. Medina
-            </span>
-          </div>
+        <div className="flex justify-center">
+          <div className="space-y-3">
+            <div className="flex items-center">
+              <LuUser className="mr-1" />
+              <span>Name:</span>
+              <span className="font-semibold text-primary ml-2">
+                Luiz F. G. Medina
+              </span>
+            </div>
 
-          <div className="flex items-center">
-            <LuMail className="mr-1" />
-            <span>Email:</span>
-            <a
-              className="font-semibold text-primary ml-1 hover:underline hover:font-bold"
-              href="mailto:mhv.geral@gmail.com"
-            >
-              mhv.geral@gmail.com
-            </a>
-          </div>
-
-          <div className="flex items-center">
-            <LuPhone className="mr-1" />
-            <span>Phone:</span>
-            <a
-              className="font-semibold text-primary ml-1 hover:underline hover:font-bold"
-              href="tel:+5531995933036"
-            >
-              +55 (31) 99593-3036
-            </a>
-          </div>
-
-          <div className="flex items-center">
-            <LuMapPin className="mr-1" />
-            <span>Country:</span>
-            <span className="flex items-center font-semibold text-primary ml-1">
-              Brazil
-              <Flag
-                code="BR"
-                className="rounded ml-2"
-                height="25px"
-                width="25px"
-              />
-            </span>
-          </div>
-
-          <div>
-            <span className="flex items-center">
-              <LuGlobe className="mr-1" />
-              Social Medias:
-            </span>
-
-            <ul className="list-disc italic text-primary space-y-1 pl-7">
+            <div className="flex items-center">
+              <LuMail className="mr-1" />
+              <span>Email:</span>
               <a
-                className="flex items-center hover:underline hover:font-semibold"
-                href="https://github.com/TheMhv"
+                className="font-semibold text-primary ml-1 hover:underline hover:font-bold"
+                href="mailto:mhv.geral@gmail.com"
               >
-                <LuGithub className="mr-1 text-text" /> Github
+                mhv.geral@gmail.com
               </a>
+            </div>
 
+            <div className="flex items-center">
+              <LuPhone className="mr-1" />
+              <span>Phone:</span>
               <a
-                className="flex items-center hover:underline hover:font-semibold"
-                href="https://www.linkedin.com/in/luizmedinaa/"
+                className="font-semibold text-primary ml-1 hover:underline hover:font-bold"
+                href="tel:+5531995933036"
               >
-                <LuLinkedin className="mr-1 text-text" /> LinkedIn
+                +55 (31) 99593-3036
               </a>
+            </div>
 
-              {/* <div className="flex items-center hover:underline hover:font-semibold">
+            <div className="flex items-center">
+              <LuMapPin className="mr-1" />
+              <span>Country:</span>
+              <span className="flex items-center font-semibold text-primary ml-1">
+                Brazil
+                <Flag
+                  code="BR"
+                  className="rounded ml-2"
+                  height="25px"
+                  width="25px"
+                />
+              </span>
+            </div>
+
+            <div>
+              <span className="flex items-center">
+                <LuGlobe className="mr-1" />
+                Social Medias:
+              </span>
+
+              <ul className="list-disc italic text-primary space-y-1 pl-7">
+                <a
+                  className="flex items-center hover:underline hover:font-semibold"
+                  href="https://github.com/TheMhv"
+                >
+                  <LuGithub className="mr-1 text-text" /> Github
+                </a>
+
+                <a
+                  className="flex items-center hover:underline hover:font-semibold"
+                  href="https://www.linkedin.com/in/luizmedinaa/"
+                >
+                  <LuLinkedin className="mr-1 text-text" /> LinkedIn
+                </a>
+
+                {/* <div className="flex items-center hover:underline hover:font-semibold">
                 <LuTwitter className="mr-1 text-text" /> Twitter
               </div> */}
 
-              <a
-                className="flex items-center hover:underline hover:font-semibold"
-                href="https://discordapp.com/users/230145118814863362"
-              >
-                <TbBrandDiscord className="mr-1 text-text" /> Discord
-              </a>
+                <a
+                  className="flex items-center hover:underline hover:font-semibold"
+                  href="https://discordapp.com/users/230145118814863362"
+                >
+                  <TbBrandDiscord className="mr-1 text-text" /> Discord
+                </a>
 
-              <a
-                className="flex items-center hover:underline hover:font-semibold"
-                href="https://api.whatsapp.com/send?phone=553195933036"
-              >
-                <TbBrandWhatsapp className="mr-1 text-text" /> WhatsApp
-              </a>
-            </ul>
+                <a
+                  className="flex items-center hover:underline hover:font-semibold"
+                  href="https://api.whatsapp.com/send?phone=553195933036"
+                >
+                  <TbBrandWhatsapp className="mr-1 text-text" /> WhatsApp
+                </a>
+              </ul>
+            </div>
           </div>
         </div>
       </>
